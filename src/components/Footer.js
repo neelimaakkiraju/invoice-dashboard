5b8dae40841a185a57aa1401cded997b7f5a82e0
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-12-24 11:20:30 +0530

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,11 @@ const Footer = () => {
   return (
     <footer className="w-full py-4 mt-auto bg-white border-t">
       <div className="container mx-auto text-center">
-        <div className="flex flex-col items-center">
-          <img src="/logo.png" alt="logo" className="h-8 w-100 mb-10"/>
+        <div className="flex flex-col items-center font-bold">
+         <h>Finance Monitor</h>
+        
         </div>
+         <p className="text-sm"> © 2026 Neelima Akkiraju. All rights reserved.</p>
       </div>
     </footer>
   );
